17f6101d2fd25207a1c6cd1f3ea75c068d57b910
mkpmmilan <prageethmilan99@gmail.com>
2022-02-23 18:50:03 +0530

    updated

--- src/db/DbConnection.java
+++ src/db/DbConnection.java
@@ -1,30 +1,30 @@
 package db;
 
-import sun.security.pkcs11.Secmod;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static DbConnection dbConnection=null;
-    private Connection connection;
+    private static DbConnection dbConnection = null;
+    private final Connection connection;
 
     private DbConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection= DriverManager.getConnection(
+        connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/Wholesale",
                 "root",
                 "1234"
         );
     }
+
     public static DbConnection getInstance() throws SQLException, ClassNotFoundException {
-        if(dbConnection==null){
-            dbConnection=new DbConnection();
+        if (dbConnection == null) {
+            dbConnection = new DbConnection();
         }
         return dbConnection;
     }
-    public Connection getConnection(){
+
+    public Connection getConnection() {
         return connection;
     }
 }
